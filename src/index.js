b806d6cad7972392ea21e0b4c079375dfa0f8a60
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-06-22 21:30:31 -0300

    setup inicial

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const pkg = require('../package.json');
+
+const port = process.env.PORT || 3000;
+
+app.set('pkg', pkg);
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+    return res.status(400).json({
+        message: err.message
+    })
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+        description: app.get('pkg').description,
+        version:  app.get('pkg').version,
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) console.error(err);
+    console.log("Server on port " + port);
+});
